7f37e1f67c6b114abaa9d1438e45475d61354f32
curso.ads <curso.ads@LAB06-67>
2022-07-06 22:00:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            for (int i = 2; i <= 100; i = i + 2) {
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
